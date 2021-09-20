0301259d4c14428bcf43c1da1d4adf6bf60fac29
Joris Patroons <git@jox.be>
2021-09-20 22:23:47 +0200

    migrate original yoman gulp-webapp site to Statiq
    upgrade Bootstrap 3 to 5, glyphicons to font-awesome
    using .NET 6 preview Razor pages for templating
    remove modernizr

---
+++ src/Touch.Statiq/Program.cs
@@ -0,0 +1,20 @@
+﻿using Statiq.App;
+using Statiq.Common;
+using Statiq.Core;
+using Statiq.Web;
+
+await Bootstrapper
+    .Factory
+    .CreateWeb(args)
+    .AddIsolatedPipeline("webfonts",
+        new CopyFiles("../lib/font-awesome/webfonts/fa-solid-**").To(f =>
+            Task.FromResult(new NormalizedPath("webfonts").Combine(f.Path.FileName)
+        )))
+    .AddIsolatedPipeline("bootstrap",
+        new CopyFiles("../lib/bootstrap/js/bootstrap.min.js").To(f =>
+            Task.FromResult(new NormalizedPath("scripts").Combine(f.Path.FileName)
+        )))
+    .RunAsync();
+
+public record Ploeg(char Letter, int Afdeling, int AfdelingId, int TeamId);
+public record Contact(string Naam, string Telefoon, string EmailGebruiker, string EmailDomein);
